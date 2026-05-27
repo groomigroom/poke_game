161ab6eae8c11777e5df5b7aef463e3405f5b5c6
groomigroom <googjohn0143@gmail.com>
2026-05-27 17:57:48 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,84 @@
+//타입들 변수 선언
+let normal = 1; //노말
+let fighting = 2; //격투
+let flying = 3; //비행
+let poison = 4; //독
+let land = 5; //땅
+let rock = 6; //바위
+let bug = 7; //벌레
+let ghost = 8; //고스트
+let steel = 9; //강철
+let fire = 10; //불꽃
+let water = 11; //물
+let grass = 12; //풀
+let electric = 13; //전기
+let esper = 14; //에스퍼
+let ice = 15; //얼음
+let dragon = 16; //드래곤
+let evil = 17; //악
+let fairy = 18; //페어리
+
+let one = "user";
+let two = "com";
+
+//상성 계산 ==> 일반 4분의 1
+
+//노말 타입일 때 상성
+if (user == normal) {
+    if (com == ghost) {
+        //고스트는 0
+        
+    }
+    else if (com == rock || com == steel) {
+        //바위 강철은 8분의 1
+    }
+    else {
+        //데미지 4분의 1
+    }
+}
+
+//격투 타입일 때 생성
+else if (user == fighting) {
+    if (com == normal || com == ice || com == rock) {
+        //노말, 얼음, 바위에 2분의 1
+    }
+    else if (com == poison || com == bug || com == flying || com == esper) {
+        //독, 벌레, 비행, 에스퍼 8분의 1
+    }
+    else if (com == ghost) {
+        //고스트는 0
+    }
+    else {//나머지는 4분의 1
+    
+    }
+}
+
+//비행 타입일 때 상성
+else if (user == flying) {
+    if (com == grass || com == fighting || com == bug) {
+        //풀, 격투, 벌레에 2분의 1
+    }
+    else if (com == electric || com == rock) {
+        //전기, 바위에 8분의 1
+    }
+    else {
+        //나머지 4분의 1
+    }       
+}
+
+//독 타입일 때 상성
+else if (user == )
+    //풀, 벌레에 2분의 1
+    //독, 땅, 바위, 고스트 8분의 1
+    //나머지 4분의 1
+
+//땅 타입일 때 상성
+
+//불꽃, 전기, 독, 바위 2분의 1
+//풀, 벌레 8분의 1
+//비행에 0
+
+//바위 타입일 때 상성
+
+//불꽃, 얼음, 비행, 벌레 2분의 1
+//격투, 땅 8분의 1
